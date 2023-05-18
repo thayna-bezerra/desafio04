23bb1acb8cbe65bf6a6ec6d05c3e3eb15e1a07ba
thayna <arraisthayna2106@gmail.com>
2023-05-18 15:48:47 -0300

    setup frontend✅

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +12,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en">
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+      <h1 className="text-4xl font-bold">
+        Sua cápsula do tempo
+      </h1>
+    </div>
   )
 }
