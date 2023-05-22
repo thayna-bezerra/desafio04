d5f7dd2ac9bc646ce847dd395b11a04c39cf86c7
thayna <arraisthayna2106@gmail.com>
2023-05-22 17:18:52 -0300

    visual structure of the home

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,20 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
-      <h1 className="text-4xl font-bold">
-        Sua cápsula do tempo
-      </h1>
-    </div>
+    <main className="grid grid-cols-2 min-h-screen">
+      {/* Left */}
+      <div className="flex flex-col items-start justify-between px-28 py-16"> 
+
+      </div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16"> 
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a {' '}
+            <a href="" className="underline hover:text-gray-50">criar agora</a>! 
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
