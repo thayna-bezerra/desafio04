dfa3811379a74bea1fdac1e2df8fbca7736c3d16
thayna <arraisthayna2106@gmail.com>
2023-05-22 19:39:14 -0300

    blur bg and stripes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,8 +2,12 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* Left */}
-      <div className="flex flex-col items-start justify-between px-28 py-16"> 
-
+      <div className="relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden border-r border-white/10"> 
+        {/*Blur*/}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"/>
+      
+        {/*Stripes*/}
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"/>
       </div>
 
       {/* Right */}
