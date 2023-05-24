e5d844b83cb7e15f838262348ba6806318c24056
thayna <arraisthayna2106@gmail.com>
2023-05-24 16:12:12 -0300

    auth

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"/>
 
         {/*Sign In*/}
-        <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
+        <a href="https://github.com/login/oauth/authorize?client_id" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
             <User className='h-5 w-5 text-gray-500' />
           </div>
